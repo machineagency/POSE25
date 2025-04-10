c722a46ec883d7c150ca22e67fcd0a9ff24ff45e
danlil666 <luo.denier@gmail.com>
2025-04-10 16:42:45 -0700

    modified sensor things

--- spectral-sensor/Seeed_files/boot.py
+++ spectral-sensor/Seeed_files/boot.py
@@ -1,3 +1,12 @@
-# import libraries for bus and board
+import pwmio
 import busio
 import board
+from adafruit_as7341 import AS7341
+
+def initialize_peripherals():
+    i2c = busio.I2C(board.IO6, board.IO5)
+    sensor = AS7341(i2c)
+    return sensor
+
+# Initialize
+sensor = initialize_peripherals()
